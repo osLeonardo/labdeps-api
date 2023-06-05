bb0059946c3eb300b542d92d80ba22940c1251ca
Bruno <bruno.lorenzoni@hotmail.com>
2023-06-05 16:09:13 -0300

    primeira parte do create de usuario (ainda não funcional)

--- frontend/src/app/components/administracao/usuario-create/usuario-create.component.ts
+++ frontend/src/app/components/administracao/usuario-create/usuario-create.component.ts
@@ -1,4 +1,9 @@
+
 import { Component } from '@angular/core';
+import { CreateUsuarios, Perfil } from '../models/create-usuarios.Model';
+import { AdministracaoService } from '../administracao.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-usuario-create',
@@ -7,4 +12,23 @@ import { Component } from '@angular/core';
 })
 export class UsuarioCreateComponent {
 
+  usuario: CreateUsuarios = {
+    login: 'bruno',
+    password: '123',
+    perfilUsuario: 0,
+    perfil: {
+      id: 0
+    }
+  }
+
+  constructor(private AdministracaoService: AdministracaoService, private router: Router,) {}
+
+  CadastrarUsuario(): void {
+    this.AdministracaoService.Cadastrar(this.usuario).subscribe(() => {
+      this.router.navigate(['/administracao'])
+     }) 
+  }
+  cancelar(): void {
+    this.router.navigate(['/administracao'])
+  }
 }
--- frontend/src/app/views/crud-administracao/crud-administracao.component.ts
+++ frontend/src/app/views/crud-administracao/crud-administracao.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crud-administracao',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class CrudAdministracaoComponent {
 
+  constructor(private router: Router) {}
+
+  navegarParaCadastrarUsuario(): void {
+
+    
+
+  }
 }
