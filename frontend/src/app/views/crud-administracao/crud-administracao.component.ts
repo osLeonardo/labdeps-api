58b8b1b8a794a05633ccf182d7c526fca2526d80
Bruno <bruno.lorenzoni@hotmail.com>
2023-06-05 13:59:27 -0300

    add CRUD-ADM

---
+++ frontend/src/app/views/crud-administracao/crud-administracao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-administracao',
+  templateUrl: './crud-administracao.component.html',
+  styleUrls: ['./crud-administracao.component.css']
+})
+export class CrudAdministracaoComponent {
+
+}
